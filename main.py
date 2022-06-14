47b93473b0abed1503e1c1a6d6663fa4205d6354
Devon Kozenieski <81592385+DevonKoz@users.noreply.github.com>
2022-06-14 09:30:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import argparse
-import json
-import linecache
 import logging
 import time
 from pathlib import Path
@@ -11,35 +9,19 @@ from win10toast import ToastNotifier
 # Sample application to trigger Windows toast notifications when new database structure change events are written to
 # a log file by IRI Ripcurrent.
 
-lines = []  # The lines that make up the current event.
 directory = ''  # The directory of the specified Ripcurrent log file.
 file = ''  # The location to the Ripcurrent log file as specified.
-current_line = 1  # Current line being processed from Ripcurrent log.
-line_number_tracker_file = None  # File to log current line number.
-line_json = None  # Python dictionary with key of line_number.
 
 
 def read(filename):
     toaster = ToastNotifier()
-    global current_line
-    line = linecache.getline(filename, current_line)
-    if line != '':
-        current_line += 1
-        line_json['line_number'] = current_line
-        with open('line_number.json', 'w') as f:
-            f.write(json.dumps(line_json))
-    while line != '':
-        lines.append(line)
-        if 'detected for table' in line:  # 'detected for table signals the end of an event.
-            text_of_lines = ''.join([str(item) for item in lines])
-            toaster.show_toast("Ripcurrent Database Structure Change Alert",
-                               f'Database structure change event detected: "{text_of_lines}".')
-            lines.clear()
-        line_json['line_number'] = current_line
-        with open('line_number.json', 'w') as f:
-            f.write(json.dumps(line_json))
-        current_line += 1
-        line = linecache.getline(filename, current_line)
+    last_line = ''
+    with open(filename) as f:
+        data = f.read().split('\n')[:-1]
+        for line in data:
+            last_line = line
+    toaster.show_toast("Ripcurrent Database Structure Change Alert",
+                       f'Database structure change event detected: "{last_line}".')
 
 
 class DirectoryWatch:
@@ -85,16 +67,5 @@ if __name__ == "__main__":
     if args.file:
         file = args.file
     directory = str(Path(file).resolve().parent)
-    try:
-        line_number_tracker_file = open('line_number.json')
-        text = line_number_tracker_file.read()
-        if text != '':
-            line_json = json.loads(text)
-            current_line = int(line_json['line_number'])
-        else:
-            line_json = {"line_number": current_line}
-        line_number_tracker_file.close()
-    except OSError as e:
-        line_json = {"line_number": current_line}
     watch = DirectoryWatch()
     watch.run(directory)
